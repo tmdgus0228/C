7b331f8c2ab5bc484ab3e1dd5d2010c48a2bbdf6
tmdgus0228 <ksh14628@naver.com>
2021-09-17 03:28:29 +0900

    Add 2주차 과제[hw1.c]

--- hw1.c/hw1.c/소스.c
+++ hw1.c/hw1.c/소스.c
@@ -1,9 +1,9 @@
-#include<stdio.h>
+ï»¿#include<stdio.h>
 
 int main(void)
 {
 	int num1, num2;
-	printf("µÎ Á¤¼ö¸¦ ÀÔ·ÂÇÏ¼¼¿ä: ");
+	printf("ë‘ ì •ìˆ˜ë¥¼ ì…ë ¥í•˜ì„¸ìš”: ");
 	scanf("%d %d", &num1, &num2);
 	printf("%d & %d = %d\n", num1, num2, num1 & num2);
 	printf("%d | %d = %d\n", num1, num2, num1 | num2);
