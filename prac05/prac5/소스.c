45be540480694ab75d6fb2f34df5e9002b731e52
tmdgus0228 <ksh14628@naver.com>
2021-10-14 17:43:42 +0900

    prac05

---
+++ prac05/prac5/소스.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main()
+{
+	/*
+	// 문제 1
+	int x1, x2, y1, y2;
+	printf("좌 상단의 x, y 좌표: ");
+	scanf("%d %d", &x1, &y1);
+	printf("우 하단의 x, y 좌표: ");
+	scanf("%d %d", &x2, &y2);
+	printf("두 점이 이루는 직사각형의 넓이는 %d입니다.\n", (x2 - x1) * (y2 - y1));
+	
+	// 문제 2
+	double num1, num2;
+	printf("두 실수를 입력하세요.: ");
+	scanf("%lf %lf", &num1, &num2);
+	printf("%lf + %lf = %lf\n", num1, num2, num1 + num2);
+	printf("%lf - %lf = %lf\n", num1, num2, num1 - num2);
+	printf("%lf * %lf = %lf\n", num1, num2, num1 * num2);
+	printf("%lf / %lf = %lf\n", num1, num2, num1 / num2);
+	
+	// 문제 4
+	int num3;
+	printf("정수를 입력하세요.: ");
+	scanf("%d", &num3);
+	printf("%c\n", num3);
+	*/
+	char asc1;
+	printf("알파벳을 입력하세요.: ");
+	scanf("%c", &asc1);
+	printf("%d", asc1);
+}
