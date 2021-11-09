c07f04c6b5323cce8593ba02029eeff52f50300f
tmdgus0228 <ksh14628@naver.com>
2021-11-09 16:40:52 +0900

    9주차 과제

---
+++ hw5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+	int arr[5];
+	int i;
+
+	printf("5개의 정수를 입력하세요: ");
+
+	for (i = 0; i < 5;i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	printf("홀수 출력: ");
+	for (i = 0;i < 5;i++)
+	{
+		if (arr[i] % 2 == 0)
+			printf("%d ", arr[i]);
+	}
+
+	printf("\n짝수 출력: ");
+	for (i = 0;i < 5;i++)
+	{
+		if (arr[i] % 2 != 0)
+			printf("%d ", arr[i]);
+	}
+}
