50f4b4c764c8a22b7b3fead903c94509dbeca675
tmdgus0228 <83226327+tmdgus0228@users.noreply.github.com>
2022-01-07 02:03:54 +0900

    Delete hw2.c directory

--- hw2.c/hw2/소스.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(void)
-{
-	float num1;
-	printf("Please enter kilometers: ");
-	scanf("%f", &num1);
-	printf("%.1f km is equal to %.1f miles.", num1, num1 / 1.609);
-}
